bc4011a20b427f96f70753bed25270d5bac519af
Isaac231 <isaacoe89@gmail.com>
2022-10-15 16:48:50 -0500

    Agregue un personaje que se puede movver

---
+++ Assets/scripts/playerMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float velocity;
+    private Vector2 movement;
+    private Rigidbody2D _rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float directionInput = Input.GetAxis("Horizontal");
+        movement = new Vector2(directionInput, 0f);
+        Debug.Log(directionInput);
+
+
+
+    }
+    void FixedUpdate()
+    {
+        float horizontalVelocity = movement.normalized.x * velocity;
+        _rigidBody.velocity = new Vector2(horizontalVelocity, _rigidBody.velocity.y);
+
+    }
+}
